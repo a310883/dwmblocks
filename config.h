c0bd5dcc259c2536d3c05d5849e4f28d03992636
Jorge Castillo <a310883@uach.mx>
2020-09-02 17:59:22 -0600

    Commented some blocks I dont use

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11},*/
+	{"",	"music",	0,	11},
 	{"",	"pacpackages",	0,	8},
 	/* {"",	"news",		0,	6}, */
 	/* {"",	"crypto",	0,	13}, */
@@ -17,10 +17,10 @@ static const Block blocks[] = {
 	/* {"",	"mailbox",	180,	12}, */
 	/* {"",	"nettraf",	1,	16}, */
 	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
+	/* {"",	"battery",	5,	3}, */
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	/* {"",	"help-icon",	0,	15}, */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
